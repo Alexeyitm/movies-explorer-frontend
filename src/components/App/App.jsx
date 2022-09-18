f4ee3b2ae56d63df8834f4f9bae46f991b020023
Alexey <ponomarevmgn@gmail.com>
2022-09-18 22:16:19 +0500

    feat: create markup promo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import Footer from './Footer/Footer';
+import Main from './Main/Main';
 
 function App() {
   return (
     <div className="app">
-      <Footer/>
+      <Main/>
     </div>
   );
 }
---
+++ src/components/App/Main/Main.jsx
@@ -0,0 +1,14 @@
+import './Main.css';
+import Promo from './Promo/Promo';
+import Footer from '../Footer/Footer';
+
+function Main() {
+  return (
+    <div className="main">
+      <Promo/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Main;
