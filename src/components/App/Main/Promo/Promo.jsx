1269996aaf05dde0e45bed975b6277734197a47c
Alexey <ponomarevmgn@gmail.com>
2022-09-19 09:54:40 +0500

    fix: fixed markup promo block

--- src/components/App/Main/Promo/Promo.jsx
+++ src/components/App/Main/Promo/Promo.jsx
@@ -3,10 +3,11 @@ import './Promo.css';
 function Promo() {
   return (
     <div className="promo">
-      <h1 className="promo__title">
-        Учебный проект студента факультета Веб-разработки.
-      </h1>
-      <div className="promo__picture">
+      <div className="promo__container">
+        <h1 className="promo__title">
+          Учебный проект студента факультета Веб-разработки.
+        </h1>
+        <div className="promo__picture"></div>
       </div>
     </div>
   );
