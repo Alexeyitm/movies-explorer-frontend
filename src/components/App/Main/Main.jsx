5128e4c09b8e8456fa3516e20acb59791996bcda
Alexey <ponomarevmgn@gmail.com>
2022-09-19 18:22:16 +0500

    feat: create markup about-project

--- src/components/App/Main/Main.jsx
+++ src/components/App/Main/Main.jsx
@@ -1,11 +1,13 @@
 import './Main.css';
 import Promo from './Promo/Promo';
 import Footer from '../Footer/Footer';
+import AboutProject from './AboutProject/AboutProject';
 
 function Main() {
   return (
     <div className="main">
       <Promo/>
+      <AboutProject/>
       <Footer/>
     </div>
   );
