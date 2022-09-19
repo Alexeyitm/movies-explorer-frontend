1b8d84d46cf128c351d5d7d7b3cf1309bdd5088d
Alexey <ponomarevmgn@gmail.com>
2022-09-19 10:01:34 +0500

    fix: fixed markup footer

--- src/components/App/Footer/Footer.jsx
+++ src/components/App/Footer/Footer.jsx
@@ -4,10 +4,12 @@ import FooterInfo from './FooterInfo/FooterInfo';
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__title">
-        Учебный проект Яндекс.Практикум х BeatFilm.
-      </p>
-      <FooterInfo/>
+      <div className="footer__container">
+        <p className="footer__title">
+          Учебный проект Яндекс.Практикум х BeatFilm.
+        </p>
+        <FooterInfo/>
+      </div>
     </footer>
   );
 }
