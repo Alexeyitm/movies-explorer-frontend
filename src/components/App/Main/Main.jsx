0dba9602793449dfab15aa7f1407a2b6205a0925
Alexey <ponomarevmgn@gmail.com>
2022-09-19 20:39:36 +0500

    feat: create markup techs

--- src/components/App/Main/Main.jsx
+++ src/components/App/Main/Main.jsx
@@ -2,12 +2,14 @@ import './Main.css';
 import Promo from './Promo/Promo';
 import Footer from '../Footer/Footer';
 import AboutProject from './AboutProject/AboutProject';
+import Techs from './Techs/Techs';
 
 function Main() {
   return (
     <div className="main">
       <Promo/>
       <AboutProject/>
+      <Techs/>
       <Footer/>
     </div>
   );
