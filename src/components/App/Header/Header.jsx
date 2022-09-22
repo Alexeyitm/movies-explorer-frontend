b135f398e28f2a436395c3c177b1e5a0b355f61f
Alexey <ponomarevmgn@gmail.com>
2022-09-22 19:06:11 +0500

    feat: complete markup header

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -1,21 +1,24 @@
 import './Header.css';
-import { NavLink } from 'react-router-dom';
 
-function Header() {
+function Header({ landing }) {
   return (
-    <footer className="header">
+    <header className={`header ${landing ? "header_landing" : ""}`}>
       <div className="header__container">
         <div className="header__logo"></div>
-        <nav className="header__nav">
+        <nav className={`header__nav-landing ${landing ? "header__nav-landing_visible" : ""}`}>
           <a className="header__link">Фильмы</a>
           <a className="header__link">Сохранённые фильмы</a>
           <a className="header__link">Аккаунт</a>
         </nav>
-        <button className="header__burger">
+        <nav className={`header__nav-main ${landing ? "" : "header__nav-main_visible"}`}>
+          <a className="header__link-registration">Регистрация</a>
+          <a className="header__link-signin">Войти</a>
+        </nav>
+        <button className={`header__burger ${landing ? "header__burger_landing" : ""}`}>
           <div className="header__burger-line"></div>
         </button>
       </div>
-    </footer>
+    </header>
   );
 };
 
